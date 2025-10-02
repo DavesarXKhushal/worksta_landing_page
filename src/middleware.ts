0bc14e5957829bc7ff0fcda5f03f3d536ebb36a4
Your Name <khushaldavesar5@gmail.com>
2025-10-02 20:18:56 +0530

    Middleware: disable Clerk locally

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,20 @@ import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isHomeRoute = createRouteMatcher(["/"]);
+const isClerkConfigured = !!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export default clerkMiddleware((auth, req) => {
-    const { userId } = auth();
+const middleware = isClerkConfigured
+    ? clerkMiddleware((auth, req) => {
+        const { userId } = auth();
 
-    // if there is user and home route is accessed, redirect to dashboard or any other protected route
-    if (userId && isHomeRoute(req)) {
-        return NextResponse.rewrite(new URL("/", req.url));
-    }
-});
+        // if there is user and home route is accessed, redirect to dashboard or any other protected route
+        if (userId && isHomeRoute(req)) {
+            return NextResponse.rewrite(new URL("/", req.url));
+        }
+    })
+    : (_req: Request) => NextResponse.next();
+
+export default middleware;
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
